4624a9f867f250cd9fbb0d391d57d2a5c733db64
AfrinRai <islamrai13@gmail.com>
2024-02-21 20:15:58 +0600

    fixed the life span, score and final score

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -24,13 +24,59 @@ function pressKeyboardButton(event) {
     if(playerPressedAlphabet === expectedAlphabet)
     {
         console.log('you got a point');
-        console.log('you have pressed ', expectedAlphabet);
+
+        
+        //for better reusable code to get & set the current score
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+        //-------------------------------------------------------------------------------------
+
+        /** 
+        //score update :
+        //1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        //2. increase the score by 1
+        const newScore = currentScore + 1;
+
+        //3. show the updated score
+        currentScoreElement.innerText = newScore;
+        */
+
+        //start new round each time call this function
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else
     {
         console.log('You missed. You lost a life');
+
+        
+        //for better reusable code to get & set the current life span
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLifeSpan = currentLife - 1;
+        setTextElementValueById('current-life', updatedLifeSpan);
+        
+        //-------------------------------------------------------------------------------------
+        /** 
+        // life span reduce
+
+        const currentLifeSpan = document.getElementById('current-life');
+        const currentLifeSpanText = currentLifeSpan.innerText;
+        const currentLife = parseInt(currentLifeSpanText);
+
+        const newLifeSpan = currentLife - 1;
+
+        currentLifeSpan.innerText = newLifeSpan;
+        */
+        if(updatedLifeSpan === 0)
+        {
+            gameOver();
+        }
     }
 
 }
@@ -53,7 +99,22 @@ function continueGame() {
 
 function play() {
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    // reset score & life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    //update final score
+    const gameLastScore = getTextElementValueById('current-score');
+    setTextElementValueById('game-score', gameLastScore);
 }
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -20,6 +20,25 @@ function removeBackgroundColorById(elementId) {
 }
 
 
+/**
+ * alternative function of getting & setting life span and score in more reusable way
+ * */
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value; 
+}
+
+
+function setTextElementValueById(elementId, value){
+     const element = document.getElementById(elementId);
+     element.innerText =  value;
+}
+
+ 
+
+
 function getARandomAlphabet() {
     // create an alphabet array
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
